13e699691729ad2125a4271834410a4be5703cd8
Jannik Birk <birk.jannik@gmail.com>
2018-09-19 23:40:21 +0200

    make some classes for shapes

--- svg.py
+++ svg.py
@@ -25,74 +25,86 @@ class Scene:
         f.write(self.render())
         f.close()
 
+class Element:
+    def __init__(self):
+        self.options = {}
 
-class Polyline:
-    def __init__(self, *points, stroke="black", stroke_width=4):
+    def option(self, key, value):
+        self.options[key] = value
+
+    def render_options(self):
+        return ' '.join(['{}="{}"'.format(k, self.options[k]) for k in self.options.keys()])
+
+class Shape(Element):
+    def __init__(self):
+        Element.__init__(self)
+
+    def stroke(self, s):
+        self.option('stroke', s)
+
+    def stroke_width(self, sw):
+        self.option('stroke-width', sw)
+
+    def fill(self, f):
+        self.option('fill', s)
+
+class Polyline(Shape):
+    def __init__(self, *points, stroke="black"):
+        Shape.__init__(self)
         self.points = points
-        self.stroke = stroke
-        self.stroke_width = stroke_width
+        self.stroke(stroke)
 
     def render(self):
         pointlist = ' '.join(['{},{}'.format(p[0],p[1]) for p in self.points])
         
-        return '<polyline points="{}" stroke="{}" stroke-width="{}" fill="none" />'\
-                .format(pointlist, self.stroke, self.stroke_width)
+        return '<polyline points="{}" {} />'\
+                .format(pointlist, self.render_options())
 
     def from_array(a, stroke="black", width=4):
         return Path(*a, stroke, width)
 
-class Rect:
-    def __init__(self, pos, width, height, fill="none", stroke="black", stroke_width=4):
+class Rect(Shape):
+    def __init__(self, pos, width, height, stroke="black"):
+        Shape.__init__(self)
         self.p = pos
         self.width = width
         self.height = height
-        self.fill = fill
-        self.stroke = stroke
-        self.stroke_width = stroke_width
+        self.stroke(stroke)
         
     def render(self):
-        return '<rect x="{}" y="{}" width="{}" height="{}" fill="{}" stroke="{}" stroke-width="{}" />'\
-                .format(self.p[0], self.p[1], self.width, self.height, self.fill, self.stroke, self.stroke_width)
+        return '<rect x="{}" y="{}" width="{}" height="{}" {} />'\
+                .format(self.p[0], self.p[1], self.width, self.height, self.render_options())
 
-class Circle:
-    def __init__(self, m, r, stroke="black", fill="none", stroke_width=4):
+class Circle(Shape):
+    def __init__(self, m, r, stroke="black"):
+        Shape.__init__(self)
         self.m = m
         self.r = r
-        self.stroke = stroke
-        self.stroke_width = stroke_width
-        self.fill = fill
+        self.stroke(stroke)
 
     def render(self):
-        return '<circle cx="{}" cy="{}" r="{}" fill="{}" stroke="{}" stroke-width="{}" />'\
-                .format(self.m[0], self.m[1], self.r, self.fill, self.stroke, self.stroke_width)
+        return '<circle cx="{}" cy="{}" r="{}" fill="{}" {} />'\
+                .format(self.m[0], self.m[1], self.r, self.render_options())
 
-class Ellipse:
-    def __init__(self, pos, width, height, stroke="black", fill="none", stroke_width=4):
+class Ellipse(Shape):
+    def __init__(self, pos, width, height, stroke="black"):
+        Shape.__init__(self)
         self.pos = pos
         self.width = width
         self.height = height
-        self.stroke = stroke
-        self.fill = fill
-        self.stroke_width = stroke_width
+        self.stroke(stroke)
 
     def render(self):
-        return '<ellipse cx="{}" cy="{}" rx="{}" ry="{}" stroke="{}" stroke-width="{}" fill="{}" />'\
-                .format(self.pos[0], self.pos[1], self.width, self.height, self.stroke, self.stroke_width, self.fill)
+        return '<ellipse cx="{}" cy="{}" rx="{}" ry="{}" {} />'\
+                .format(self.pos[0], self.pos[1], self.width, self.height, self.render_options())
 
-class Line:
-    def __init__(self, p1, p2, stroke="black", stroke_width=4):
+class Line(Shape):
+    def __init__(self, p1, p2, stroke="black"):
+        Shape.__init__(self)
         self.p1 = p1
         self.p2 = p2
-        self.stroke = stroke
-        self.stroke_width = stroke_width
+        self.stroke(stroke)
 
     def render(self):
-        return '<line x1="{}" y2="{}" x2="{}" y2="{}" stroke="{}" stroke-width="{}" />'\
-                .format(self.p1[0], self.p1[1], self.p2[0], self.p2[1], self.stroke, self.stroke_width)
-
-
-
-class Text:
-    def __init__(self, p, msg):
-        self.p = p
-        self.msg = msg
+        return '<line x1="{}" y1="{}" x2="{}" y2="{}" {} />'\
+                .format(self.p1[0], self.p1[1], self.p2[0], self.p2[1], self.render_options())
